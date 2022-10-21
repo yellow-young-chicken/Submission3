c9e0047b4c38ce7e31e16214b7119c664c12a9e2
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2022-10-21 11:45:08 +0000

    10/21

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
@@ -27,6 +28,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'homes#top'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, ]
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
   delete 'books/:id' => 'books#destroy', as: 'book_destroy'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
+  root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
